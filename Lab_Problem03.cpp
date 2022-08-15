ba6760be0d1a9c456d05b0b18d25762522697705
Arman Hossain <64675035+Arman170616@users.noreply.github.com>
2022-08-15 23:18:54 +0600

    Update Lab_Problem03.cpp

--- Lab_Problem03.cpp
+++ Lab_Problem03.cpp
@@ -3,7 +3,7 @@ Problem Statement_
 
 How many  users can be supported for 0.5% blocking probability for the following number of trunked channel in blocked calls cleaned system? 
     (a)1, (b)5, (c) 10, (d)20 (e)100 . 
-Assume each user generates 0.1 Erlamgs of Traffic.
+Assume each user generates 0.1 Erlangs of Traffic.
 */
 
 #include<bits/stdc++.h>
@@ -26,3 +26,42 @@ int main(){
     cout<<"(d) The Total Number of Users, U = "<<floor((Ad/Au))<<"users"<<endl;
     cout<<"(e) The Total Number of Users, U = "<<floor((Ae/Au))<<"users"<<endl;
 }
+
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// double A(int tc){
+    
+//     if(tc ==1){
+        
+//         return 0.005;
+//     }
+//     else if(tc == 5){
+//         return 1.13;
+//     }
+    
+//     else if(tc==10){
+//         return 3.96;
+        
+//     }
+//     else if(tc==20){
+//         return 11.1;
+//     }
+//     else if(tc==100){
+//         return 80.9;
+//     }
+// }
+
+// int main(){
+//     double Au = 0.1;
+//     double GOS = (0.5/100);
+//     double c[] ={1, 5, 10, 20, 100};
+//     for(auto it: c){
+//         cout<<"total supported users for available channel c =" <<it<<"is : "; 
+//         double U = A(it)/Au;
+//         cout<<U<<" \nby taking ceiling value: "<<ceil(U)<<endl;
+//         cout<<endl;
+//     }
+// }
