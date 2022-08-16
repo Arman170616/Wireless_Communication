1606ab8fa81b221e9ecb6250fb58aef8eb6dc5d1
Arman Hossain <64675035+Arman170616@users.noreply.github.com>
2022-08-16 09:21:06 +0600

    Update Lab_problem04.cpp

--- Lab_problem04.cpp
+++ Lab_problem04.cpp
@@ -4,68 +4,37 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-
-int main(){
-    double population, gos, Au, c, t, lam, A, U, cell, Sa, Sb, Sc;
-    population = 2000000.00;
-    gos= 0.02;
-    lam = 2.00;
-    t = 3.00;
-    Au = lam*(t/60.00);
-    
-    //system A
-    
-    cout<<"System A"<<endl;
-    cell = 394;
-    c = 19;
-    A = 12;  // Arlang B Table
-    U = floor(A/Au);
-    Sa = (U*cell);
-    cout<<"The total Number of users per cell= "<<U<<" users"<<endl;
-    cout<<"The total Number of subscribers in system A = "<<Sa<<endl;
-    
-    
-    //system B
-    
-    cout<<"System B"<<endl;
-    cell = 98;
-    c = 57;
-    A = 45;  // Arlang B Table
-    U = floor(A/Au);
-    Sb = (U*cell);
-    cout<<"The total Number of users per cell= "<<U<<" users"<<endl;
-    cout<<"The total Number of subscribers in system B = "<<Sb<<endl; // B = 44100
-    
-    
-    
-    //system C
-    
-    cout<<"System C"<<endl;
-    cell = 49;
-    c = 100;
-    A = 88;  // Arlang B Table
-    U = floor(A/Au);
-    Sc = (U*cell);
-    cout<<"The total Number of users per cell= "<<U<<" users"<<endl;
-    cout<<"The total Number of subscribers in system C = "<<Sc<<endl; // c=43120
-    
-    
-    cout<<"The cellular subscribers of three Systems="<<Sa+Sb+Sc<<endl;
-    cout<<"percentage of penetration:"<<endl;
-    
-    
-    cout<<"In system A, the percentage of market penetration = "<< Sa*100/population<<endl;
-    cout<<"In system B, the percentage of market penetration = "<< Sb*100/population<<endl;
-    cout<<"In system C, the percentage of market penetration = "<< Sc*100/population<<endl;
-    
-    cout<<"The percentage of market penetration of the three system combined = "<< (Sa+Sb+Sc)*100/population<<endl;
-    
-    
-    return 0;
-
-    
+double Au = (2.0*3.0)/60.0;
+double GOS = (2/100);
+double C[]={19, 57, 100};
+double cells[]={394, 98, 49};
+double A(int tc){
+    if(tc==19){
+        return 12.0;
+    }
+    else if(tc==57){
+        return 45.0;
+    }
+    else if(tc==100){
+        return 88.0;
+    }
 }
+void calculate(int i){
+    double U = A(C[i])/Au;
+    cout<<U<<" \nby taking ceiling value : "<<ceil(U)<<endl;
+    cout<<"It can support total "<<U*cells[i]<<" users"<<endl;
+    cout<<"Which is "<<(U*cells[i]/2e6)*100<<"% of the total population."<<endl;
+    cout<<endl;
+}
+main(){
+    cout<<"Total supported users for system A per cell is : ";
+    calculate(0);
+    cout<<"Total supported users for system B per cell is : ";
+    calculate(1);
+    cout<<"Total supported users for system C per cell is : ";
+    calculate(2);
+}
+
 
 
 
