00a9901b41493d42e319ab0092ca8a9ea9c28104
Arman Hossain <64675035+Arman170616@users.noreply.github.com>
2022-08-16 08:16:14 +0600

    Create Lab_problem09.cpp

---
+++ Lab_problem09.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout<<"(a). ";
+    double peak_deviation = 8*10;
+    cout<<"Peak frequency deviation = "<<peak_deviation<<endl;
+
+    cout<<"(b). ";
+    double FM_index = (peak_deviation)/4;
+    cout<<"Frequency modulation index = "<<FM_index<<endl;
+
+    cout<<"(c). ";
+    double PM_index = (10*8);
+    cout<<"Phase modulation index = "<<PM_index<<endl;
+
+}
+
+
+// (a). Peak frequency deviation = 80
+// (b). Frequency modulation index = 20
+// (c). Phase modulation index = 80
